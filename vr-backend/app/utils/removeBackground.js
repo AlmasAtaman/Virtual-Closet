635203548be0dee29337ec3f9b1e64846591a3bb
Almas Ataman <almasataman.cs@gmail.com>
2025-09-13 13:31:51 -0400

    Fixed removeBackground.js to use the enviroment variable

--- vr-backend/app/utils/removeBackground.js
+++ vr-backend/app/utils/removeBackground.js
@@ -1,3 +1,4 @@
+// vr-backend/app/utils/removeBackground.js
 import axios from "axios";
 import FormData from "form-data";
 import fs from "fs";
@@ -6,9 +7,15 @@ export async function removeBackground(localImagePath) {
   const form = new FormData();
   form.append("file", fs.createReadStream(localImagePath));
 
-  const response = await axios.post("http://127.0.0.1:9000/remove-bg/", form, {
+  // Use environment variable instead of hardcoded localhost
+  const PYTHON_SERVICE_URL = process.env.PYTHON_SERVICE_URL || "http://127.0.0.1:9000";
+  
+  console.log(`Calling background removal service at: ${PYTHON_SERVICE_URL}`);
+
+  const response = await axios.post(`${PYTHON_SERVICE_URL}/remove-bg/`, form, {
     headers: form.getHeaders(),
     responseType: "stream",
+    timeout: 60000, // 60 second timeout for AI processing
   });
 
   const outputPath = localImagePath.replace(/(\.\w+)$/, "_nobg.png");
