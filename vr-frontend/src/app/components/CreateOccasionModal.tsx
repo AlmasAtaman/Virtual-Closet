614d23dc547e71d8ecbce5dae316dc6341241acd
Almas Ataman <almasataman.cs@gmail.com>
2025-09-21 13:22:11 -0400

    Testing left adjustment

--- vr-frontend/src/app/components/CreateOccasionModal.tsx
+++ vr-frontend/src/app/components/CreateOccasionModal.tsx
@@ -324,7 +324,7 @@ export default function CreateOccasionModal({ show, onCloseAction, onOccasionCre
                                           let adjustedLeft = item.left ?? 50;
                                           
                                           // Move items more to the left and center them
-                                          adjustedLeft = adjustedLeft - 100; // Move everything 10% to the left
+                                          adjustedLeft = adjustedLeft - 65; // Move everything 10% to the left
                                           
                                           return (
                                             <Image
